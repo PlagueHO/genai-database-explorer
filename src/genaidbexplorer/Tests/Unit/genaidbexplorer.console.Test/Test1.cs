386918432ffd234e2a923671729d5eabfa859574
Daniel Scott-Raynsford <plagueho@gmail.com>
2024-10-17 20:57:53 +1300

    Add initial implementation of genaidbexplorer.console with unit tests

---
+++ src/genaidbexplorer/Tests/Unit/genaidbexplorer.console.Test/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace genaidbexplorer.console.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ src/genaidbexplorer/genaidbexplorer.console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace genaidbexplorer.console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
