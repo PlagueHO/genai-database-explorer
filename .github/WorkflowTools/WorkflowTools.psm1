3c32ca0f7034e24952461afb2a2acadeecabbae3
Daniel Scott-Raynsford <plagueho@gmail.com>
2025-08-24 11:29:21 +1200

    CHANGE: Refactor function import for cross-platform support
    - Updated function import method to use cross-platform compatible approach.
    - Improved readability by defining public path and listing functions explicitly.

--- .github/WorkflowTools/WorkflowTools.psm1
+++ .github/WorkflowTools/WorkflowTools.psm1
@@ -1,4 +1,9 @@
+#Requires -Version 7
 #Requires -Modules Az.CognitiveServices, Az.Accounts, Az.Sql, Az.Storage, Az.CosmosDB
 
-# Import the functions
-. $PSScriptRoot\Public\*.ps1
+# dotsource/import the functions in a cross-platform way
+$publicPath = Join-Path -Path $PSScriptRoot -ChildPath 'Public'
+$publicFunctionsList = Get-ChildItem -Path $publicPath -Filter '*.ps1' -File -ErrorAction SilentlyContinue
+$publicFunctionsList | ForEach-Object {
+    . $_.FullName
+}
